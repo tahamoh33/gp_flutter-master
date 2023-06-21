e63c313ec64b65c6c617ea425efcb6049233ee92
tm_33 <totmoh333@gmail.com>
2023-06-21 20:14:39 +0300

    Updated App Bar

--- lib/Screens/Authentication/signup.dart
+++ lib/Screens/Authentication/signup.dart
@@ -26,7 +26,7 @@ class _SignupPageState extends State<SignupPage> {
   //FirebaseAuth instance = FirebaseAuth.instance;
   bool isPasswordVisible = true;
   bool isConfirmPasswordVisible = true;
-
+  bool isChecked=false;
   @override
   void dispose() {
     _email.dispose();
@@ -192,14 +192,24 @@ class _SignupPageState extends State<SignupPage> {
                 ),
                 Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                   Checkbox(
-                    value: false,
-                    onChanged: (value) => false,
+                    value: isChecked,
+                    activeColor: Colors.blue,
+                    onChanged: (newBool) {
+                      setState(() {
+                        isChecked=newBool!;
+                      });
+                    },
                   ),
-                  Text("Agree to the terms and conditions",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 12,
-                          color: Colors.black)),
+                  TextButton(onPressed: (){
+                    showDialog(context: context, builder: (context) => AlertDialog(
+                    title:const Text("Agree to terms and conditions"),
+                      content:SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                          child: Text("Please read these terms and conditions (terms and conditions, terms) carefully before using Eye diseases classification app .Conditions of useBy using this website, you certify that you have read and reviewed this Agreement and that you agree to comply with its terms. If you do not want to be bound by the terms of this Agreement, you are advised to stop using the website accordingly. We only grants use and access of this website, its products, and its services to those who have accepted its terms.Privacy policyBefore you continue using our website, we advise you to read our privacy policy regarding our user data collection. It will help you better understand our practices.Age restrictionYou must be at least 18 (eighteen) years of age before you can use this website. By using this website, you warrant that you are at least 18 years of age and you may legally adhere to this Agreement. We assume no responsibility for liabilities related to age misrepresentation.Intellectual propertyYou agree that all materials, products, and services provided on this website are the property, its affiliates, directors, officers, employees, agents, suppliers, or licensors including all copyrights, trade secrets, trademarks, patents, and other intellectual property. You also agree that you will not reproduce or redistribute , intellectual property in any way, including electronic, digital, or new trademark registrations.You grant a royalty-free and non-exclusive license to display, use, copy, transmit, and broadcast the content you upload and publish. For issues regarding intellectual property claims, you should contact the company in order to come to an agreement.User accountsAs a user of this website, you may be asked to register with us and provide private information. You are responsible for ensuring the accuracy of this information, and you are responsible for maintaining the safety and security of your identifying information. You are also responsible for all activities that occur under your account or password.If you think there are any possible issues regarding the security of your account on the website, inform us immediately so we may address them accordingly.We reserve all rights to terminate accounts, edit or remove content and cancel orders at our sole discretion.Applicable lawBy using this website, you agree that the laws of the [your location], without regard to principles of conflict laws, will govern these terms and conditions, or any dispute of any sort that might come between and you, or its business partners and associates.DisputesAny dispute related in any way to your use of this website or to products you purchase from us shall be arbitrated by state or federal court [your location] and you consent to exclusive jurisdiction and venue of such courts.IndemnificationYou agree to indemnify and its affiliates and hold  harmless against legal claims and demands that may arise from your use or misuse of our services. We reserve the right to select our own legal counsel.Limitation on liabilityWe are not liable for any damages that may occur to you as a result of your misuse of our website.We reserve the right to edit, modify, and change this Agreement at any time. We shall let our users know of these changes through electronic mail. This Agreement is an understanding between [company name] and the user, and this supersedes and replaces all prior agreements regarding the use of this website.")) ,
+
+                    ) );
+                  }, child: Text("Agree to terms and conditions"))
+
                 ]),
 
                 SizedBox(
