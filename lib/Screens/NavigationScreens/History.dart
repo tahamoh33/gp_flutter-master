87e0cd0e4865df8422ee932007098b2d17d9583b
tm_33 <totmoh333@gmail.com>
2023-06-23 15:51:36 +0300

    Updated App Bar

--- lib/Screens/NavigationScreens/History.dart
+++ lib/Screens/NavigationScreens/History.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:trial1/Screens/NavigationScreens/DetectionScreen.dart';
 
 import '../../CustomWidgets/CustomCard.dart';
 import '../../CustomWidgets/customFile.dart';
@@ -70,12 +71,17 @@ class _HistoryState extends State<History> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("History",
+        centerTitle: true,
+        leading:IconButton(
+          onPressed:(){ Navigator.push(context, MaterialPageRoute(builder: (context)=>DetectionScreen()));},
+          icon: Icon(Icons.arrow_back_sharp),
+        ),
+        title: Text("History",
           style: TextStyle(
               fontSize: 25,
               color: Colors.blue
           ), )
-        ),
+
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
